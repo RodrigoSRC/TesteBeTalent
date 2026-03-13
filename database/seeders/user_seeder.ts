535f88b6117517be75c036e78ba8fb203c25b335
Rodrigo <clancoto2015@hotmail.com>
2026-03-12 21:31:45 -0300

    fix: move sensitive data to environment variables for improved security

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -1,14 +1,15 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import User from '#models/user'
+import env from '#start/env'
 
 export default class UserSeeder extends BaseSeeder {
   async run() {
     // Write your database queries inside the run method
     await User.updateOrCreate(
-      { email: 'admin@betalent.tech' },
+      { email: env.get('ADMIN_EMAIL') },
       {
-        email: 'admin@betalent.tech',
-        password: 'secret123',
+        email: env.get('ADMIN_EMAIL'),
+        password: env.get('ADMIN_PASSWORD'),
         role: 'admin',
       }
     )
